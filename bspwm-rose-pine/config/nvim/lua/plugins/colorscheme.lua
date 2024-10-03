e7e4c4b8e1bcb79d2904e77d9fea6deb0e599bc9
gentootoaster <lobachev.arseniy@gmail.com>
2024-10-03 23:18:05 +0500

    Add files via upload

---
+++ bspwm-rose-pine/config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add rose-pine
+  { "rose-pine/neovim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  }
+}
